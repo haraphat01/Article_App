53dcf3181045c8d63e5e23d5568feaf19cd31b36
haraphat01 <haraphat@gmail.com>
2020-09-05 19:50:47 +0100

    Worked on the category path

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,46 @@
 class CategoriesController < ApplicationController
-    def index
-        @categories = Category.all
-        @liked_article = Article.highest_vote.first
-      end
-    
-      def new
-        @category = Category.new
-      end
+  def index
+    @categories = Category.all
+    @liked_article = Article.highest_vote.first
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = 'Category was created successfully'
+      redirect_to root_path
+    else
+      flash[:alert] = 'An error occurred'
+      render 'new'
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:success] = 'Category name was updated successfully'
+      redirect_to category_path(@category)
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    @category = Category.find(params[:id])
+    @category_articles = @category.articles.ordered_by_most_recent
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
