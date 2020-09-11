7e577c338a1f5a4079844a296339ec72d656f30f
haraphat01 <haraphat@gmail.com>
2020-09-11 11:26:12 +0100

    Fixed lint errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-# rubocop:disable Style/DoubleNegation
+# rubocop:disable Style/DoubleNegation, Lint/RedundantCopDisableDirective
 class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?
 
@@ -17,4 +17,4 @@ class ApplicationController < ActionController::Base
     redirect_to root_path
   end
 end
-# rubocop:disable Style/DoubleNegation
+# rubocop:enable Style/DoubleNegation, Lint/RedundantCopDisableDirective
